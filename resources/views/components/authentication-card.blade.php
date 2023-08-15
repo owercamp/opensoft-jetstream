c341370baedd5abebc6fe92e98891677c6ba7d5e
Ower A Campos <owerion22@gmail.com>
2023-08-15 16:05:41 -0500

    feat: se añade versión y copyright a cabecera

--- resources/views/components/authentication-card.blade.php
+++ resources/views/components/authentication-card.blade.php
@@ -1,6 +1,16 @@
 <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-    <div>
-        {{ $logo }}
+    <div class="flex justify-between max-w-xl">
+      <div class="mx-3.5">
+          {{ $logo }}
+      </div>
+      <div class="mx-3.5 py-3">
+        <x-label class="capitalize">
+          {{ __('version') }}: {{ config('app.version') }}
+        </x-label>
+        <x-label>
+          {{ __('Copyright © Javapri') }}
+        </x-label>
+      </div>
     </div>
 
     <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
