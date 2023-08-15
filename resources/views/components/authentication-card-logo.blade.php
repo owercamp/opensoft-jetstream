0a2761fba722aecc89d660e38d21e3cd95897677
Ower A Campos <owerion22@gmail.com>
2023-08-15 08:48:15 -0500

    feat: se agrega prop en el atributo href para ser dinamico

--- resources/views/components/authentication-card-logo.blade.php
+++ resources/views/components/authentication-card-logo.blade.php
@@ -1,3 +1,4 @@
-<a href="{{ url('/') }}">
+@props(['url' => route('login')])
+<a {{ $attributes->merge(['href' => $url]) }}>
   <img src="{{ asset('img/shortlogo.gif') }}" class="img-opensoft" alt="{{ config('app.lastname', 'OPENSOFT') }}">
 </a>
