47c028f7d0325e3f04f2ca877ef1eb78fcb82f0e
Ower A. Campos <owerion22@gmail.com>
2023-08-25 17:04:35 -0500

    feat: cambio de la clase bg-gray-100 -> bg-orange-300 para el tema light

--- resources/views/components/dropdown-link.blade.php
+++ resources/views/components/dropdown-link.blade.php
@@ -3,7 +3,7 @@
 @php
 $classes = ($active ?? false)
             ? 'inline-flex items-center px-4 py-2 border-b-2 border-indigo-400 dark:border-indigo-600 text-sm font-medium leading-5 text-gray-900 dark:text-gray-100 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out w-full'
-            : 'block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-indigo-700 focus:outline-none focus:bg-gray-100 dark:focus:bg-indigo-700 transition duration-150 ease-in-out';
+            : 'block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 dark:text-gray-300 hover:bg-orange-300 dark:hover:bg-indigo-700 focus:outline-none focus:bg-orange-300 dark:focus:bg-indigo-700 transition duration-150 ease-in-out';
 @endphp
 
 
