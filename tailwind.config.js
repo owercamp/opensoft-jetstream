69adb264f9e4d2ea18d6350c56ffc8c581626de2
Ower A. Campos <owerion22@gmail.com>
2023-08-25 16:56:51 -0500

    feat: implementación de flowbite en theme dark/light

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,10 @@ export default {
         './vendor/laravel/jetstream/**/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        './node_modules/flowbite/**/*.js',
     ],
 
+    darkMode: 'class',
     theme: {
         extend: {
             fontFamily: {
@@ -19,5 +21,5 @@ export default {
         },
     },
 
-    plugins: [forms, typography],
+    plugins: [forms, typography,require('flowbite/plugin')],
 };
